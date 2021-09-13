84b28decf6277bfca1c384636dd0115a4cf2cb31
ENEMYForYou <72756415+ENEMYForYou@users.noreply.github.com>
2021-09-13 21:46:59 +0300

    Update buttonUp.js

--- js/buttonUp.js
+++ js/buttonUp.js
@@ -1,4 +1,24 @@
-$(function() {
+$(function () {
+ $(document).ready(function(){
+   var scroll_pos = 0;
+   var footer = $('footer');
+    $(document).scroll(function() { 
+       w = Math.floor( $(window).scrollTop() );
+   $('.remove').css( 'background-color', '' ); 
+    if(w<=$('.home_btn').offset().top){
+         $('.home_btn').css( 'color', 'black' );
+    }else{
+         $('.home_btn').css( 'color', 'blue' );
+    }
+});
+    //     scroll_top = $(this).scrollTop();
+    //     if(scroll_top > 640) { 
+    //         $(".home_btn").css('color', 'white');
+    //     } else {
+    //         $(".home_btn").css('color', 'black');
+    //     }
+    // });
+});
   $(window).scroll(function() {
   if($(this).scrollTop() != 0) {
   $('.home_btn').fadeIn();
