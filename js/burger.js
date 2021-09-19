02df3c00321670c2df6421e7a45ffdbf329d2e27
ENEMYForYou <enemyforyou3@gmail.com>
2021-09-19 12:22:49 +0300

    Burger Bag fix
    Fixed bags w/ JS

--- js/burger.js
+++ js/burger.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
         $('.header__burger,.header_nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.header_nav_item').click(function(event) {
+        $('.header__burger,.header_nav').removeClass('active');
+        $('body').removeClass('lock');
+    });
 });
